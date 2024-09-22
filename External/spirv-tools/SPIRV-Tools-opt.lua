c3758a23855a86899d8ad5694111c27f5f4f384f
Stulu <derstulu@gmail.com>
2024-09-22 23:50:12 +0200

    Initial Commit

---
+++ External/spirv-tools/SPIRV-Tools-opt.lua
@@ -0,0 +1,58 @@
+project "SPIRV-Tools-opt"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "ENABLE_SPIRV",
+        "ENABLE_HLSL",
+        "SPIRV_CHECK_CONTEXT",
+    }
+
+    files
+    {
+        "source/opt/**.h",
+        "source/opt/**.cpp",
+    }
+
+    includedirs
+    {
+        "",
+        "include",
+        "external/spirv-headers/include",
+    }
+
+    links{
+        "SPIRV-Tools-static"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+        defines { 
+            "GLSLLANG_OSINCLUDE_WIN32",
+            "SPIRV_WINDOWS",
+            "SPIRV_COLOR_TERMINAL",
+        }
+        includedirs { "build-windows" }
+        files { "OSDependent/Windows/**.cpp"}
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        optimize "off"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        symbols "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
